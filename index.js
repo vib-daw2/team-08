04377ffbf46496d615cc4eda01c088e785636ebc
lace8guti <lace8guti@gmail.com>
2024-01-15 13:17:15 +0100

    crear partida y fotos preguntes

--- index.js
+++ index.js
@@ -2,116 +2,131 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
+
 const app = express();
 const httpServer = createServer(app);
 
+
 app.use(express.static("public"));
 
+
 const io = new Server(httpServer, {});
+
+
 const fs = require("fs");
 // Cargar les preguntes desde l'arxiu preguntes.json
 const preguntes = JSON.parse(fs.readFileSync("preguntes.json", "utf-8"));
 //generar un identificador únic per cada partida creada
 const { v4: uuidv4 } = require('uuid');
-
+const users = [];
 io.on("connection", (socket) => {
-  
     console.log('Connectat un client...')
-    //socket.data.nickname = "alice";
+   //socket.data.nickname = "alice";
 
-    //socket obtenir nickname
-    socket.on("nickname", function(data) {
-            console.log(data.nickname)
-            
-            socket.data.nickname = data.nickname;
-            const redirectUrl = "/home.html";
-            // respondre al que ha enviat
-            socket.emit("nickname rebut",{"response":"ok", redirectUrl})
-    })
 
-    //socket obtenir usuaris
-    socket.on("get users", function(data) {
-        const users = [];
-      
-        for (let [id, socket] of io.of("/").sockets) {
-            
-          users.push({
-            userID: id,
-            username: socket.data.nickname || "NoNickname",
-          });
-        }
-      
-        io.emit("users", users);
-        console.log("llista d'usuaris enviada");
-      });
-      //socket crear partida, filtra les preguntes segons els requisits del form (quantitat i topics)
-      socket.on("crear partida", function(configuracioPartida) {
-        //console.log("hola");
-        try {
-            // Manejar la lógica de creación de partida aquí
-            const { title, quantity, topics, nickname } = configuracioPartida;
-    
-            // Inicializar un objeto para almacenar preguntas por tema
-            const preguntasPorTema = {};
-    
-            // Filtrar preguntas según la configuración recibida y agruparlas por tema
-            preguntes.forEach((pregunta) => {
-                const tema = pregunta.modalitat.toLowerCase();
-                if (topics.includes(tema)) {
-                    preguntasPorTema[tema] = preguntasPorTema[tema] || [];
-                    preguntasPorTema[tema].push(pregunta);
-                }
-            });
-    
-            // Inicializar un array para almacenar las preguntas finales
-            const preguntesPartida = [];
-    
-            // Calcular la cantidad de preguntas por tema
-            const cantidadPorTema = Math.floor(quantity / topics.length);
-    
-            // Seleccionar preguntas de cada tema según la cantidad calculada
-            topics.forEach((tema) => {
-                const preguntasDelTema = preguntasPorTema[tema] || [];
-                preguntesPartida.push(...preguntasDelTema.slice(0, cantidadPorTema));
-            });
-    
-            // Si hay preguntas restantes, seleccionar al azar de los temas disponibles
-            const preguntasRestantes = quantity - preguntesPartida.length;
-            if (preguntasRestantes > 0) {
-                const temasDisponibles = topics.filter((tema) => preguntasPorTema[tema]?.length > cantidadPorTema);
-                for (let i = 0; i < preguntasRestantes; i++) {
-                    const temaAleatorio = temasDisponibles[Math.floor(Math.random() * temasDisponibles.length)];
-                    const preguntasDelTema = preguntasPorTema[temaAleatorio] || [];
-                    preguntesPartida.push(preguntasDelTema[Math.floor(Math.random() * preguntasDelTema.length)]);
-                }
-            }
-            const idPartida = uuidv4();
-            // Asociar la sala con el identificador único
-            const salaPartida = `partida-${idPartida}`;
-            socket.join(salaPartida);
-            // Enviar preguntas al cliente
-            //console.log(nickname);
-            socket.emit("preguntes partida", { idPartida, preguntesPartida, nickname });
-        } catch (error) {
-            console.error("Error al procesar la solicitud de creación de partida:", error);
-        }
-    });
-    
-    
+   //socket obtenir nickname
+   socket.on("nickname", function(data) {
+          
+           socket.nickname = data.nickname;
+           users.push({
+               userID: socket.id,
+               username: socket.nickname
+           });
+           const redirectUrl = "/home.html";
+           // respondre al que ha enviat
+           socket.emit("nickname rebut",{"response":"ok", redirectUrl})
+   })
+
+
+
+
+   socket.on("get users", function() {
+       socket.emit("users", { users });
+   });
+
+
+     //socket crear partida, filtra les preguntes segons els requisits del form (quantitat i topics)
+     socket.on("crear partida", function(configuracioPartida) {
+       //console.log("hola");
+       try {
+           // Manejar la lógica de creación de partida aquí
+           const { title, quantity, topics, nickname } = configuracioPartida;
+  
+           // Inicializar un objeto para almacenar preguntas por tema
+           const preguntasPorTema = {};
+  
+           // Filtrar preguntas según la configuración recibida y agruparlas por tema
+           preguntes.forEach((pregunta) => {
+               const tema = pregunta.modalitat.toLowerCase();
+               if (topics.includes(tema)) {
+                   preguntasPorTema[tema] = preguntasPorTema[tema] || [];
+                   preguntasPorTema[tema].push(pregunta);
+               }
+           });
+  
+           // Inicializar un array para almacenar las preguntas finales
+           const preguntesPartida = [];
+  
+           // Calcular la cantidad de preguntas por tema
+           const cantidadPorTema = Math.floor(quantity / topics.length);
+  
+           // Seleccionar preguntas de cada tema según la cantidad calculada
+           topics.forEach((tema) => {
+               const preguntasDelTema = preguntasPorTema[tema] || [];
+               preguntesPartida.push(...preguntasDelTema.slice(0, cantidadPorTema));
+           });
+  
+           // Si hay preguntas restantes, seleccionar al azar de los temas disponibles
+           const preguntasRestantes = quantity - preguntesPartida.length;
+           if (preguntasRestantes > 0) {
+               const temasDisponibles = topics.filter((tema) => preguntasPorTema[tema]?.length > cantidadPorTema);
+               for (let i = 0; i < preguntasRestantes; i++) {
+                   const temaAleatorio = temasDisponibles[Math.floor(Math.random() * temasDisponibles.length)];
+                   const preguntasDelTema = preguntasPorTema[temaAleatorio] || [];
+                   preguntesPartida.push(preguntasDelTema[Math.floor(Math.random() * preguntasDelTema.length)]);
+               }
+           }
+           const idPartida = uuidv4();
+           // Asociar la sala con el identificador único
+           const salaPartida = `partida-${idPartida}`;
+           socket.join(salaPartida);
+           // Enviar preguntas al cliente
+           //console.log(nickname);
+           socket.emit("preguntes partida", { idPartida, preguntesPartida, nickname });
+       } catch (error) {
+           console.error("Error al procesar la sol·licitud de creació de la partida:", error);
+       }
+   });
+   socket.on("join game", function(data) {
+       const { idPartida, nickname } = data;
+       const salaPartida = 'partida-${idPartida}';
+       socket.join(salaPartida);
+       console.log("unit a la sala")
+
+
+
+
+   });
+  
+
 
 });
+
 
 /*
 var comptador = 1;
 
+
 setInterval(() => {
-    console.log('envio missatge a tots els clients')
-    io.emit('salutacio', comptador);
-    comptador++;
+   console.log('envio missatge a tots els clients')
+   io.emit('salutacio', comptador);
+   comptador++;
 }, 5000);
 */
 
 
+
+
 httpServer.listen(3000, ()=>
-    console.log(`Server listening at http://localhost:3000`)
+   console.log(`Server listening at http://localhost:3000`)
 );
--- public/main.js
+++ public/main.js
@@ -1,136 +1,189 @@
-
 const socket = io();
+
 
 const createButton = document.getElementById("createButton");
 const nicknameInput = document.getElementById("nicknameInput");
 const sendButton = document.getElementById("sendButton");
 
+
 if (sendButton) {
-    sendButton.addEventListener("click", send);
+   sendButton.addEventListener("click", send);
 }
+
+
+
+
+
+
+
+
 function send() {
-    const nickname = nicknameInput.value;
+   const nickname = nicknameInput.value;
 
-    // Guardar el nickname en el almacenamiento local
-    localStorage.setItem("nickname", nickname);
 
-    socket.emit("nickname", { nickname });
+   // Guardar el nickname en el almacenamiento local
+   localStorage.setItem("nickname", nickname);
+
+
+   socket.emit("nickname", { nickname });
 }
+
 
 socket.on('nickname rebut', function(data) {
 
-    console.log(data)
-    if (data.redirectUrl) {
-        //redirigir a la pàgina que indica el servidor(home)
-        window.location.href = data.redirectUrl;
-    }
-    
+
+   console.log(data)
+   if (data.redirectUrl) {
+       //redirigir a la pàgina que indica el servidor(home)
+       window.location.href = data.redirectUrl;
+   }
+  
 })
 
-socket.on("connect", function() {
-    console.log("Conexió amb el servidor");
-    //socket.emit("get users");
+
+
+
+socket.on("connect", function () {
+   console.log("Connexió amb el servidor");
 });
+
+
+//cridar "get users"
+//socket.emit("get users");
+
+
+// Gestionar la resposta amb tots els usuaris
+socket.on("users", function(data) {
+   const userList = data;
+   console.log("Llista d'usuaris:", userList);
+});
+
+
+
+
+
 
 
 
 socket.on('salutacio', function(data) {
 
-    console.log(data)
+
+   console.log(data)
+
 
 })
 
+
 if (createButton) {
 createButton.addEventListener("click", function() {
-    // Redirigir a la pàgina createGame.html
-    window.location.href = "createGame.html";
+   // Redirigir a la pàgina createGame.html
+   window.location.href = "createGame.html";
 });
 }
 
 
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Verificar si estem en createGame.html
-    if (window.location.href.endsWith("createGame.html")) {
-       // Obtener el nickname del almacenamiento local
-       const nicknameLocal = localStorage.getItem("nickname") || "";
+   // Verificar si estem en createGame.html
+   if (window.location.href.endsWith("createGame.html")) {
+      // Obtener el nickname del almacenamiento local
+      const nicknameLocal = localStorage.getItem("nickname") || "";
 
-       // Completar automáticamente el campo de entrada de título con el nickname
-       const titleInput = document.getElementById("title");
-       if (titleInput) {
-           titleInput.value = "Partida de " + nicknameLocal;
-       }
-        document.getElementById("createGameForm").addEventListener("submit", function (event) {
-            event.preventDefault(); 
-            console.log("Formulari enviat");
 
-            // Guardar les dades en el formulari
-            const formData = {
-                title: document.getElementById("title").value,
-                quantity: document.getElementById("quantity").value,
-                topics: Array.from(document.querySelectorAll('input[name="topic"]:checked')).map(topic => topic.value),
-                nickname: localStorage.getItem("nickname") || "" 
-            };
+      // Completar automáticamente el campo de entrada de título con el nickname
+      const titleInput = document.getElementById("title");
+      if (titleInput) {
+          titleInput.value = "Partida de " + nicknameLocal;
+      }
+       document.getElementById("createGameForm").addEventListener("submit", function (event) {
+           event.preventDefault();
+           console.log("Formulari enviat");
 
-            // Emitir un event al servidor amb les dades del formulari
-            socket.emit("crear partida", formData);
-        });
-    } else {
-        //console.log("No estas en createGame.html");
-    }
 
-    //rep la partida configurada
-    socket.on("preguntes partida", function(dataPartida) {
-        const { idPartida, preguntesPartida, nickname } = dataPartida;
-        console.log(dataPartida)
-        // Redirigir a la página lobby.html con el identificador único en la URL
-        const lobbyUrl = `http://localhost:3000/lobby.html?partida=${dataPartida.idPartida}&nickname=${dataPartida.nickname}`;
-        console.log(lobbyUrl);
-        window.location.href = lobbyUrl;
-    });
-    
+           // Guardar les dades en el formulari
+           const formData = {
+               title: document.getElementById("title").value,
+               quantity: document.getElementById("quantity").value,
+               topics: Array.from(document.querySelectorAll('input[name="topic"]:checked')).map(topic => topic.value),
+               nickname: localStorage.getItem("nickname") || ""
+           };
+
+
+           // Emitir un event al servidor amb les dades del formulari
+           socket.emit("crear partida", formData);
+       });
+   } else {
+       //console.log("No estas en createGame.html");
+   }
+
+
+   //rep la partida configurada
+   socket.on("preguntes partida", function(dataPartida) {
+       const { idPartida, preguntesPartida, nickname } = dataPartida;
+       console.log(dataPartida)
+       // Redirigir a la página lobby.html con el identificador único en la URL
+       const lobbyUrl = `http://localhost:3000/lobby.html?partida=${dataPartida.idPartida}&nickname=${dataPartida.nickname}`;
+       console.log(lobbyUrl);
+       window.location.href = lobbyUrl;
+   });
+  
 });
+
 
 //assignar un títol
 document.addEventListener("DOMContentLoaded", function () {
-    const params = new URLSearchParams(window.location.search);
-    const nickname = params.get("nickname");
-    const storedNickname = localStorage.getItem("nickname") || "";
-    console.log(storedNickname);
-    // Modificar el título en lobby.html
-    const titleLobby = document.getElementById("titleLobby");
-    if (titleLobby && nickname) {
-        titleLobby.innerText = "Partida de " + nickname;
+   const params = new URLSearchParams(window.location.search);
+   const nickname = params.get("nickname");
+   const storedNickname = localStorage.getItem("nickname") || "";
+   //console.log(storedNickname);
+   // Modificar el título en lobby.html
+   const titleLobby = document.getElementById("titleLobby");
+   if (titleLobby && nickname) {
+       titleLobby.innerText = "Partida de " + nickname;
 
-        // Ocultar el botón de empezar partida si el nickname actual no coincide
-        const startButton = document.getElementById("start-button");
-        if (storedNickname !== nickname) {
-            startButton.style.display = "none";
-        }
-    }
+
+       // Ocultar el botón de empezar partida si el nickname actual no coincide
+       const startButton = document.getElementById("start-button");
+       if (storedNickname !== nickname) {
+           startButton.style.display = "none";
+       }
+   }
 });
 
 
+
+
+//entrar a una sala a traves de la URL
 document.addEventListener("DOMContentLoaded", function () {
-    // Verificar si estas en home.html
-    if (window.location.href.endsWith("home.html")) {
-       
-        // Obtener elementos del DOM
-        const linkInput = document.getElementById("linkInput");
-        const entrarButton = document.getElementById("entrarButton");
+   // Verificar si estas en home.html
+   if (window.location.href.endsWith("home.html")) {
+     
+       // Obtener elementos del DOM
+       const linkInput = document.getElementById("linkInput");
+       const entrarButton = document.getElementById("entrarButton");
 
-        // Agregar un evento de clic al botón "Entrar"
-        entrarButton.addEventListener("click", function () {
-            // Obtener la URL ingresada por el usuario
-            const lobbyUrl = linkInput.value;
 
-            // Redirigir a la lobby
-            window.location.href = lobbyUrl;
-        });
-    } else {
-        //console.log("No estás en home.html");
-    }
+       // Agregar un evento de clic al botón "Entrar"
+       entrarButton.addEventListener("click", function () {
+           // Obtener la URL ingresada por el usuario
+           const lobbyUrl = linkInput.value;
+
+
+           // Redirigir a la lobby
+           window.location.href = lobbyUrl;
+       });
+   } else {
+       //console.log("No estás en home.html");
+   }
 });
 
 
+// Extraer parámetros de la URL
+const urlParams = new URLSearchParams(window.location.search);
+const idPartida = urlParams.get('partida');
+const nickname = urlParams.get('nickname');
 
 
+// Enviar mensaje al servidor para unirse a la sala de la partida
+socket.emit("join game", { idPartida, nickname });
